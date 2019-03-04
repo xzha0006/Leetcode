61b4e5ae017b8fcc2302629e7d3212d68fa70c3e
xuanzhang <xzha0006@student.monash.edu>
2019-03-04 23:57:09 +1100

    polished up the 00039 solution

--- Java/0039_CombinationSum.java
+++ Java/0039_CombinationSum.java
@@ -2,21 +2,23 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> ans = new ArrayList<>();
         ArrayList<Integer> combination = new ArrayList<>();
+        Arrays.sort(candidates);
         dfs(candidates, combination, 0, target, ans);
         return ans;
     }
     
     private void dfs(int[] candidates, ArrayList<Integer> combination, int currentLength, int target, List<List<Integer>> ans) {
-        int sum = combination.stream().mapToInt(Integer::intValue).sum();
-        if (sum == target) {
+        //int sum = combination.stream().mapToInt(Integer::intValue).sum();
+        if (0 == target) {
             ans.add((ArrayList) combination.clone());
             return;
-        } else if (sum > target) {
+        } else if (0 > target) {
             return;
         }
-        for (int i = currentLength; i < candidates.length; i++) {           
+        for (int i = currentLength; i < candidates.length; i++) {
+            if (candidates[i] > target) break;
             combination.add(candidates[i]);
-            dfs(candidates, combination, i, target, ans);
+            dfs(candidates, combination, i, target - candidates[i], ans);
             combination.remove(new Integer(candidates[i]));
         }
     }
