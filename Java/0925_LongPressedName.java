7bc07e07b1f97a24688186f69a8e16422ad1a14f
xuanzhang <xzha0006@student.monash.edu>
2019-03-22 21:55:52 +1100

    0322 James 0925 long pressed name two point. a bit hard.

---
+++ Java/0925_LongPressedName.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isLongPressedName(String name, String typed) {
+        int i = 0, j = 0;
+        for (j = 0; j < typed.length(); ++j) {
+            if (i < name.length() && name.charAt(i) == typed.charAt(j)) ++i;
+            else if (j == 0 || typed.charAt(j) != typed.charAt(j - 1))
+                return false;
+        }
+        return i == name.length();
+    }
+}
