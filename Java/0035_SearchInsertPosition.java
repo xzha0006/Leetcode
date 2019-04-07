9a884c5bbf9505cf3d92fd9ce4c09d235fa4288b
xuanzhang <xzha0006@student.monash.edu>
2019-04-07 23:17:03 +1000

    0028 binary search

---
+++ Java/0035_SearchInsertPosition.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int left = 0, right = n - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                return mid;
+            }
+        }
+        if (nums[left] == target) return left;
+        return nums[left] > target ? left : left + 1;
+    }
+}
