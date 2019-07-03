727fb4e7d398230527c54aeaa87c9ce13966eeb6
xuanzhang <zhangxuan.james@gmail.com>
2019-07-03 22:37:44 +1000

    0136 xor operation

--- Java/0136_SingleNumber.java
+++ Java/0136_SingleNumber.java
@@ -14,3 +14,13 @@ class Solution {
         return 0;
     }
 }
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res = 0;
+        for (int num: nums) {
+            res ^= num;
+        }
+        return res;
+    }
+}
