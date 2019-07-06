6788557a67cc16f340e3df5700cc3890e40b03df
xuanzhang <zhangxuan.james@gmail.com>
2019-07-06 16:34:51 +1000

    0171 easy question

--- Java/0171_ExcelSheetColumnNumber.java
+++ Java/0171_ExcelSheetColumnNumber.java
@@ -1,1 +1,12 @@
-tmp
+class Solution {
+    public int titleToNumber(String s) {
+        int n = s.length();
+        char[] chars = s.toCharArray();
+        int res = 0;
+        for (char c: chars) {
+            res *= 26;
+            res += c - 'A' + 1;
+        }
+        return res;
+    }
+}
