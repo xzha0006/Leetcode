d7f13aed3f65cde76588c32a179b32a7ca4c70ec
xuanzhang <zhangxuan.james@gmail.com>
2019-07-07 23:32:38 +1000

    modified 0125 better solution

--- Java/0125_ValidPalindrome.java
+++ Java/0125_ValidPalindrome.java
@@ -2,10 +2,10 @@ class Solution {
     public boolean isPalindrome(String s) {
         int n = s.length();
         if (n == 0) return true;
-        char[] chars = s.toUpperCase().toCharArray();
+        char[] chars = s.toCharArray();
         int left = 0, right = n - 1;
         while (left < right) {
-            if (chars[left] == chars[right]) {
+            if (equal(chars[left], chars[right])) {
                 ++left;
                 --right;
             }
@@ -25,6 +25,14 @@ class Solution {
     }
     
     private boolean isValid(char c) {
-        return (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+        return (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z');
+    }
+    
+    private boolean equal(char a, char b) {
+        if (((a >= 'A' && a <= 'Z') || (a >= 'a' && a <= 'z')) && ((b >= 'A' && b <= 'Z') || (b >= 'a' && b <= 'z'))) {
+            return a == b || Math.abs(a - b) == ('a' - 'A');
+        } else {
+            return a == b;
+        }
     }
 }
