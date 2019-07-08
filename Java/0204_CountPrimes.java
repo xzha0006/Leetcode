1bce608ddbf133cbe26374b59b16ac0f58df351e
xuanzhang <zhangxuan.james@gmail.com>
2019-07-08 22:44:35 +1000

    0204 count primes good question

--- Java/0204_CountPrimes.java
+++ Java/0204_CountPrimes.java
@@ -1,2 +1,23 @@
-tmp
+class Solution {
+    public int countPrimes(int n) {
+        if (n < 3) return 0;
+        if (n == 3) return 1;
+        int[] primeMap = new int[n];
+        setNonPrime(n, 3, primeMap);
+        int count = 2;
+        for (int j = 5; j < n; j += 2) {
+            if (primeMap[j] == 0) {
+                setNonPrime(n, j, primeMap);
+                ++count;
+            }
+        }
+        return count;
+    }
+    
+    private void setNonPrime(int n, int num, int[] primeMap) {
+        for (int i = num; i < Math.ceil((double) n / num); ++i) {
+            primeMap[i * num] = 1;
+        }
+    }
 
+}
