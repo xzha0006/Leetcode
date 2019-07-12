4161b0b7907434678837707ebb9b6faee2ca0b66
xuanzhang <zhangxuan.james@gmail.com>
2019-07-12 19:20:48 +1000

    0836 easy

--- Java/0836_RectangleOverlap.java
+++ Java/0836_RectangleOverlap.java
@@ -1,1 +1,7 @@
-tmp
+class Solution {
+    public boolean isRectangleOverlap(int[] rec1, int[] rec2) {
+        int x1 = 0, y1 = 1, x2 = 2, y2 = 3;
+        return !(rec1[x2] <= rec2[x1] || rec1[x1] >= rec2[x2] || rec1[y1] >= rec2[y2] || rec1[y2] <= rec2[y1]) ;
+    }
+
+}
