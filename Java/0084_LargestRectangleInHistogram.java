c5ceea2b0d6edf02f6068490a67f854d6e2dd069
xuanzhang <zhangxuan.james@gmail.com>
2019-07-15 22:10:52 +1000

    0084 dp solution

--- Java/0084_LargestRectangleInHistogram.java
+++ Java/0084_LargestRectangleInHistogram.java
@@ -1,4 +1,31 @@
 dp solution:
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] lessLeft = new int[n];
+        int[] lessRight = new int[n];
+        int l, r;
+        for (int i = 0; i < n; ++i) {
+            l = i - 1;
+            while (l >= 0 && heights[l] >= heights[i]) {
+                l = lessLeft[l];
+            }   
+            lessLeft[i] = l;
+        }
+        for (int j = n - 1; j >= 0; --j) {
+            r = j + 1;
+            while (r < n && heights[r] >= heights[j]) {
+                r = lessRight[r];
+            }
+            lessRight[j] = r;
+        }
+        int maxArea = 0;
+        for (int k = 0; k < n; ++k) {
+            maxArea = Math.max(maxArea, (lessRight[k] - lessLeft[k] - 1) * heights[k]);
+        }
+        return maxArea;
+    }
+}
 
 class Solution {
     public int largestRectangleArea(int[] heights) {
