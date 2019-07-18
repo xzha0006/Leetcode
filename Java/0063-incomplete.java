218a11f0a738fd31e884e5d132f3ab072bb56579
xuanzhang <zhangxuan.james@gmail.com>
2019-07-18 15:36:31 +1000

    0063 dp easy

--- Java/0063-incomplete.java
+++ Java/0063-incomplete.java
@@ -3,6 +3,31 @@ class Solution {
         if (obstacleGrid.length == 0) return 0;
         int m = obstacleGrid.length;
         int n = obstacleGrid[0].length;
-        int[][] solution = new int[]
+        int[][] solution = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            if (obstacleGrid[i][0] != 1) {
+                solution[i][0] = 1;
+            } 
+            else {
+                break;
+            }
+        }
+        for (int j = 0; j < n; ++j) {
+            if (obstacleGrid[0][j] != 1) {
+                solution[0][j] = 1;
+            } 
+            else {
+                break;
+            }
+        }
+        
+        for (int k = 1; k < m; ++k) {
+            for (int l = 1; l < n; ++l) {
+                if (obstacleGrid[k][l] != 1) {
+                    solution[k][l] = solution[k - 1][l] + solution[k][l - 1];
+                }
+            }
+        }
+        return solution[m - 1][n - 1];
     }
 }
