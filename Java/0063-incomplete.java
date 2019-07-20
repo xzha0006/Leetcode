b6c89d0640495b0aab47772014d480b572720657
xuanzhang <zhangxuan.james@gmail.com>
2019-07-20 16:19:15 +1000

    0063 recursion

--- Java/0063-incomplete.java
+++ Java/0063-incomplete.java
@@ -31,3 +31,32 @@ class Solution {
         return solution[m - 1][n - 1];
     }
 }
+
+class Solution {
+    private int[][] memo;
+    private int m, n;
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid.length == 0) return 0;
+        m = obstacleGrid.length;
+        n = obstacleGrid[0].length;
+        memo = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            Arrays.fill(memo[i], -1);
+        }
+        return findPathNum(obstacleGrid, m - 1, n - 1);
+    }
+    
+    private int findPathNum(int[][] obstacleGrid, int i, int j) {
+        if (obstacleGrid[i][j] == 1) return 0;
+        if (i == 0 && j == 0) return 1;
+        int res = 0;
+        if (i - 1 >= 0 && obstacleGrid[i - 1][j] != 1) {
+            res += memo[i - 1][j] != -1 ? memo[i - 1][j] : findPathNum(obstacleGrid, i - 1, j);
+        }
+        if (j - 1 >= 0 && obstacleGrid[i][j - 1] != 1) {
+            res += memo[i][j - 1] != -1 ? memo[i][j - 1] : findPathNum(obstacleGrid, i, j - 1);
+        }
+        memo[i][j] = res;
+        return  res;
+    }
+}
