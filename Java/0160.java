3afd29a73de9e42de5406bc97739c5d843919407
xuanzhang <zhangxuan.james@gmail.com>
2019-07-23 21:55:42 +1000

    160 two pointer solution. genius

--- Java/0160.java
+++ Java/0160.java
@@ -1,3 +1,14 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        while (p1 != p2) {
+            p1 = p1 != null ? p1.next : headB;
+            p2 = p2 != null ? p2.next : headA;
+        }
+        return p1;
+    }
+}
 /**
  * Definition for singly-linked list.
  * public class ListNode {
