f0edf234d925160021744fcdf186146d54018f78
xuanzhang <zhangxuan.james@gmail.com>
2019-09-03 22:34:54 +1000

    0221 dp solution

--- Java/0221_MaximalSquare.java
+++ Java/0221_MaximalSquare.java
@@ -1,5 +1,32 @@
 class Solution {
     public int maximalSquare(char[][] matrix) {
+        if (matrix == null || matrix.length == 0) return 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int maxSize = -1;
+        for (int i = 0; i < m; ++i) {
+            dp[i][0] = matrix[i][0] - '0';
+            maxSize = Math.max(dp[i][0], maxSize);
+        }
+        for (int i = 0; i < n; ++i) {
+            dp[0][i] = matrix[0][i] - '0';
+            maxSize = Math.max(dp[0][i], maxSize);
+        }
+        
+        for (int j = 1; j < m; ++j) {
+            for (int k = 1; k < n; ++k) {
+                dp[j][k] = matrix[j][k] == '1' ? Math.min(Math.min(dp[j - 1][k], dp[j][k - 1]), dp[j - 1][k - 1]) + 1 : 0;
+                maxSize = Math.max(maxSize, dp[j][k]);
+            }
+        }
+    return maxSize * maxSize;
+        
+    }
+}
+
+class Solution {
+    public int maximalSquare(char[][] matrix) {
         if (matrix.length == 0) return 0;
         int m = matrix.length;
         int n = matrix[0].length;
