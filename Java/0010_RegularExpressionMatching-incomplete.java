c6cd16d81e61223c7f741c0d9bd6158ed61f9223
xuanzhang <zhangxuan.james@gmail.com>
2019-09-03 11:31:58 +1000

    0010 regex matching. good question

--- Java/0010_RegularExpressionMatching-incomplete.java
+++ Java/0010_RegularExpressionMatching-incomplete.java
@@ -1,3 +1,24 @@
+// dp solution
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int m = s.length(), n = p.length();
+        boolean[][] dp = new boolean[m+1][n+1];
+        dp[m][n] = true;
+        for (int i = m; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                boolean firstMatch = i < m && (p.charAt(j) == '.' || p.charAt(j) == s.charAt(i));
+                if (j + 1 < n && p.charAt(j + 1) == '*') {
+                    dp[i][j] = dp[i][j + 2] || (firstMatch && dp[i + 1][j]);
+                }
+                else {
+                    dp[i][j] = firstMatch && dp[i + 1][j + 1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
+// recursive solution
 class Solution {
     public boolean isMatch(String s, String p) {
         if (s == null || p == null) return false;
