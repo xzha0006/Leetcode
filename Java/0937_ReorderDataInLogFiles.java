1ac4682588d7aaa7ade4b16455f1e35398299698
xuanzhang <zhangxuan.james@gmail.com>
2019-09-28 09:05:20 +1100

    0937 string sort. Quick sort solution

--- Java/0937_ReorderDataInLogFiles.java
+++ Java/0937_ReorderDataInLogFiles.java
@@ -1,3 +1,41 @@
+// quick sort
+class Solution {
+    public String[] reorderLogFiles(String[] logs) {
+        int index = logs.length - 1;
+        for (int i = logs.length - 1; i >= 0; i--) {
+            String cur = logs[i].substring(logs[i].indexOf(" ") + 1);
+            if (cur.charAt(0) <= '9') {
+                swap(logs, index--, i);
+            }
+        }
+        quickSort(logs, 0, index);
+        return logs;
+    }
+    
+    // Sort arr
+    private void quickSort(String[] arr, int start, int end) {
+        if (start <= end) {
+            String pivot = arr[end].substring(arr[end].indexOf(" ") + 1);
+            int index = start;
+            for (int i = start; i < end; i++) {
+                String cur = arr[i].substring(arr[i].indexOf(" ") + 1);
+                if (pivot.compareTo(cur) > 0 || ((pivot.compareTo(cur) == 0 && arr[end].compareTo(arr[i]) > 0))) {
+                    swap(arr, index++, i);
+                } 
+            }
+            swap(arr, index, end);
+            quickSort(arr, start, index - 1);
+            quickSort(arr, index + 1, end);
+        }
+    }
+    
+    private void swap(String[] arr, int old, int nnew) {
+        String tmp = arr[old];
+        arr[old] = arr[nnew];
+        arr[nnew] = tmp;
+    }
+}
+// common solution
 class Solution {
     private static class LogComparator implements Comparator<String> {
         @Override
