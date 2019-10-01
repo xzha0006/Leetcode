e2ff863fd4ebe4da3fc5c0cd4602415453397ddd
xuanzhang <zhangxuan.james@gmail.com>
2019-10-02 09:05:20 +1100

    0973 k closest points-quick sort solution

--- Java/0973_KClosestPointsToOrigin.java
+++ Java/0973_KClosestPointsToOrigin.java
@@ -1,3 +1,41 @@
+// quick sort solution
+class Solution {
+    int[][] points;
+    public int[][] kClosest(int[][] points, int K) {
+        this.points = points;
+        sort(0, points.length - 1, K);
+        return Arrays.copyOfRange(points, 0, K);
+    }
+    
+    public void sort(int start, int end, int K) {
+        if (start < end) {
+            int[] pivot = points[end];
+            int index = start;
+            for (int i = start; i < end; ++i) {
+                if (compare(points[i], pivot) <= 0) {
+                    swap(index++, i);
+                }
+            }
+            swap(index, end);
+            if (index - start + 1 < K) {
+                sort(index + 1, end, K - index + start - 1);
+            }
+            else if (index - start + 1 > K) {
+                sort(start, index - 1, K);
+            }
+        }
+        
+    }
+    private void swap(int old, int nw) {
+        int[] tmp = points[old];
+        points[old] = points[nw];
+        points[nw] = tmp;
+    }
+    
+    private int compare(int[] p1, int[] p2) {
+        return (p1[0] * p1[0] + p1[1] * p1[1]) - (p2[0] * p2[0] + p2[1] * p2[1]);
+    }
+}
 // qp solution
 class Solution {
     public int[][] kClosest(int[][] points, int K) {
